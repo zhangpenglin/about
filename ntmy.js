04089e61b30ab2b3d5a85b88bc25fe79d048e492
Zhang Penglin <zhangpenglin@users.noreply.github.com>
2019-09-08 22:09:27 +0800

    Update ntmy.js

--- ntmy.js
+++ ntmy.js
@@ -2,7 +2,7 @@ var u = navigator.userAgent;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 var app = new CallApp({
-    scheme: 'ntmy://',
+    scheme: 'ntmy://?id=123',
     timeout: 1800,
     autoCall: true // 自动唤起
 });
