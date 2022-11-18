287f0e81ca440700a2379ef2842f4cdc08b16693
zhang penglin <penglin.zhang@phyworks.cn>
2022-11-18 17:10:16 +0800

    add a2hs

--- ntmy.js
+++
@@ -1,8 +0,0 @@
-var u = navigator.userAgent;
-var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-var app = new CallApp({
-    scheme: 'ntmy://action/card/123',
-    timeout: 1800,
-    autoCall: true // 自动唤起
-});
